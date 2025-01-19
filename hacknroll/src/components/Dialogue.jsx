4b166ef80559de3d318ff97ee9209ed7857710aa
AWYS7 <andreawong9041@gmail.com>
2025-01-19 09:45:38 +0800

    meh

--- hacknroll/src/components/Dialogue.jsx
+++ hacknroll/src/components/Dialogue.jsx
@@ -1,41 +1,46 @@
-import { useState } from 'react'
-import '../styles/dialogue.css'
+import { useState } from "react";
+import "../styles/dialogue.css";
 
-const Dialogue = ({dialogue, onClose}) => {
-    const [currentDialogueIndex, setCurrentDialogueIndex] = useState(0)
+const Dialogue = ({ dialogue, onClose }) => {
+  const [currentDialogueIndex, setCurrentDialogueIndex] = useState(0);
 
-    const handleNext = () => {
-        if (currentDialogueIndex < dialogue.length - 1) {
-          setCurrentDialogueIndex(currentDialogueIndex + 1);
-        }
-    };
+  const handleNext = () => {
+    if (currentDialogueIndex < dialogue.length - 1) {
+      setCurrentDialogueIndex(currentDialogueIndex + 1);
+    }
+  };
 
-    const handlePrevious = () => {
-        if (currentDialogueIndex >= 1) {
-          setCurrentDialogueIndex(currentDialogueIndex - 1);
-        }
-    };
+  const handlePrevious = () => {
+    if (currentDialogueIndex >= 1) {
+      setCurrentDialogueIndex(currentDialogueIndex - 1);
+    }
+  };
 
-    const currentDialogue = dialogue[currentDialogueIndex];
-    
-    return (
-        <div className="dialogue-container">
-          <div className="cat-image">
-            {currentDialogue.image && (
-              <img src={currentDialogue.image} alt={currentDialogue.character} />
-            )}
-          </div>
+  const currentDialogue = dialogue[currentDialogueIndex];
 
-          <div className="dialogue-box">
-            <h2>{currentDialogue.character}</h2>
-            <p>{currentDialogue.text}</p>
-          </div>
-          
-          {currentDialogue.id === 1 ? null : <button onClick={handlePrevious}>{'<<'}</button>}
-            {currentDialogue.id === 6 ? <button onClick={onClose}>{'Close'}</button> : 
-                <button onClick={handleNext}>{'>>'}</button>}
-        </div>
-      );
+  return (
+    <div className="dialogue-container">
+      <div className="cat-image">
+        {currentDialogue.image && (
+          <img src={currentDialogue.image} alt={currentDialogue.character} />
+        )}
+      </div>
+
+      <div className="dialogue-box">
+        <h2>{currentDialogue.character}</h2>
+        <p>{currentDialogue.text}</p>
+      </div>
+
+      {currentDialogue.id === 1 ? null : (
+        <button onClick={handlePrevious}>{"<<"}</button>
+      )}
+      {currentDialogue.id === 6 ? (
+        <button onClick={onClose}>{"Close"}</button>
+      ) : (
+        <button onClick={handleNext}>{">>"}</button>
+      )}
+    </div>
+  );
 };
 
 export default Dialogue;
